e4d4f683524ca5e86bb4e20711f40afce0627fa7
anastasia-gutova <56114375+anastasia-gutova@users.noreply.github.com>
2021-06-16 20:47:07 +0300

    Удалила методы when, IF, добавила данные и lodash

--- index.js
+++ index.js
@@ -1,154 +1,103 @@
-var tab1Array;
-
-function forEach(arraySet, callback) {
-  for(let i=0; i<arraySet.length; i += 1) {
-    callback(i);
-  }
-}
-
-function withArrayCopy(array, callback) {
-  var arrayCopy = createArrayCopy(array);
-  callback(arrayCopy);
-  return arrayCopy;
-}
-
-function createArrayCopy(array) {
-  var copyArray = new Array(array.length);
-  forEach(copyArray, function(index){
-    copyArray[index] = array[index].slice();
-  })
-  return copyArray
-}
-
-function getAllElementsByTypeRowCol(type, rowNum = "", colNum = "", strictMode = true) {
-  return $("[id" + (strictMode ? "" : "*") + "=" + type + rowNum + (colNum !== "" ? "_" + colNum : "" ) +"]")
-}
-
-function getInputValue(inputSelector) {
-  return $(inputSelector).val();
-}
-
-function setInputValue(inputSelector, newValue) {
-  $(inputSelector).val(newValue);
-}
-
-function tableRowsProcessing() {
-  tab1Array = fillArray(cleanArray());
-
-  forEach([1, 2, 3, 4, 5], function (index) {
-    forEach(getAllElementsByTypeRowCol('floatInput', index + 1, '', false), function (column) {
-      inputsActions(el, tab1Array, index, column);
+function tableRowsProcessing() { // действие
+  updateArrayView(DATA);
+  DATA.forEach((row, rowIdx) => {
+    row.forEach((_el, colIdx) => {
+      inputsActions($(`[id="floatInput${rowIdx + 1}_${colIdx + 1}"]`), rowIdx, colIdx);
     });
   });
 }
 
-function inputsActions(el, array, rowIndex, column) {
-  $(el).click(function () { tableOnElementClick(this) });
-  $(el).change(function () { tableOnElementChange(this) });
-  $(el).keyup(function () { tableOnElementKeyUp(this, array, rowIndex, column) });
+function inputsActions(el, rowIndex, column) { // действие
+  $(el).click(function () { tableOnElementClick(rowIndex, column) });
+  $(el).change(function () { tableOnElementChange(this, rowIndex, column) });
+  $(el).keyup(function () { tableOnElementKeyUp(this, rowIndex, column) });
 }
 
-function tableOnElementKeyUp(element, array, rowIndex, column) {
-    array = setArrayValue( array, rowIndex, column, parseFloat(getInputValue(element)));
-    updateConnectedCells(array, rowIndex, column);
-}
-
-function fillArray(array) {
-  return withArrayCopy(array, function(arrayCopy) {
-    forEach([1, 2, 3, 4, 5], function (index) {
-      forEach(getAllElementsByTypeRowCol('floatInput', index + 1, '', false), function (column) {
-        arrayCopy[index][column] = parseFloat(getInputValue(el));
-      });
-    });
-  })
-}
-
-// Очищение массива значений
-function cleanArray() {
-  var array = new Array(5);
-  array[0] = new Array(10);
-  array[1] = new Array(10);
-  array[2] = new Array(10);
-  array[3] = new Array(10);
-  array[4] = new Array(10);
-  return array;
+function tableOnElementKeyUp(element, rowIndex, column) { // действие
+  DATA = updateArrayValue(rowIndex, column, element.value, DATA);
+  DATA = updateConnectedValues(DATA, rowIndex, column);
+  updateArrayView(DATA);
 }
 
 // Метод для очищения нулевого значения в input
-function tableOnElementClick(element) {
-  when(getInputValue(element) == 0, function () {
-    setInputValue(element, '');
-  });
-}
-
-function isAddZero(value) {
-  return value.indexOf('.') == 0 || value.length == 0
-}
-
-// Дописывает 0 для ситуации ввода числа ".5"
-function tableOnElementChange(element) {
-  var currentElementValue = getInputValue(element);
-  when (isAddZero(currentElementValue), function() {
-    setInputValue(element, "0" + currentElementValue);
-  });
-}
-
-function getArrayValue(array, row, col) {
-  return array[row][col];
-}
-
-function setArrayValue(array, row, col, newValue) {
-  return withArrayCopy(array, function (arrayCopy) {
-    arrayCopy[row][col] = newValue;
-  });
-}
-
-function updateConnectedCells(array, rowIndex, column) {
-  return withArrayCopy(array, function () {
-    when(rowIndex == 0 || rowIndex == 2 || rowIndex == 4, function () {
-      var columnNum = (column + 1);
-      var resultRow2 = getNewCalculatedValues(getArrayValue(arrayCopy, 0, column), getArrayValue(arrayCopy, 2, column), getArrayValue(arrayCopy, 4, column), 0);
-      var resultRow4 = getNewCalculatedValues(getArrayValue(arrayCopy, 0, column), getArrayValue(arrayCopy, 2, column), getArrayValue(arrayCopy, 4, column), 1);
-      setInputValue(getAllElementsByTypeRowCol('floatInput', 2, columnNum), resultRow2);
-      setInputValue(getAllElementsByTypeRowCol('floatInput', 4, columnNum), resultRow4);
-  
-      arrayCopy = setArrayValue(arrayCopy, 1, column, resultRow2);
-      arrayCopy = setArrayValue(arrayCopy, 3, column, resultRow4);
-    });
-  })
-}
-
-function when(condition, callback) {
-  if (condition) {
-    return callback();
-  } 
-}
-
-function IF(condition, callbackTrue, callbackFalse) {
-  if (condition) {
-    return callbackTrue();
-  } else {
-    return callbackFalse();
+function tableOnElementClick(row, col) { // действие
+  if (DATA[row][col] == 0) {
+    DATA = updateArrayValue(row, col, '', DATA);
+    updateArrayView(DATA);
   }
 }
 
-function getNewCalculatedValues(valueRow1, valueRow3, valueRow5, parType) {
-  return IF(parType == 0,
-    function () {
-      return getNonFinite(devideTwoNumbers(valueRow1, valueRow3));
-  },
-    function () {
-      return getNonFinite(devideTwoNumbers(valueRow1, valueRow3) - devideTwoNumbers(valueRow1, valueRow5))
-  });
+// Дописывает 0 для ситуации ввода числа ".5"
+function tableOnElementChange(element, row, col) { // действие
+  DATA = updateArrayValue(row, col, element.value, DATA);
+  var currentElementValue = DATA[row][col];
+  if (isAddZero(currentElementValue)) {
+    DATA = updateArrayValue(row, col, "0" + currentElementValue, DATA);
+  }
+  updateArrayView(DATA);
 }
 
-function devideTwoNumbers (number1, number2) {
+function isAddZero(value) { // вычисление
+  return value.startsWith('.') || value.length == 0
+}
+
+function updateConnectedValues(array, rowIndex, column) { // вычисление
+  return withArrayCopy(array, function (arrayCopy) {
+    if (rowIndex == 0 || rowIndex == 2 || rowIndex == 4) {
+      arrayCopy[1][column] = getNewCalculatedValues(arrayCopy[0][column], arrayCopy[2][column], arrayCopy[4][column], 0);
+      arrayCopy[3][column] = getNewCalculatedValues(arrayCopy[0][column], arrayCopy[2][column], arrayCopy[4][column], 1);
+    }
+    return arrayCopy
+  })
+}
+
+function getNewCalculatedValues(valueRow1, valueRow3, valueRow5, parType) { // вычисление
+  if (parType == 0) {
+    return getNonFinite(devideTwoNumbers(valueRow1, valueRow3));
+  } else {
+    return getNonFinite(devideTwoNumbers(valueRow1, valueRow3) - devideTwoNumbers(valueRow1, valueRow5))
+  }
+}
+
+function devideTwoNumbers(number1, number2) { // вычисление
   var notNanValue1 = isNaN(number1) ? 0 : number1;
   var notNanValue2 = isNaN(number2) ? 0 : number2;
   return notNanValue1 / notNanValue2;
 }
 
 // Возвращает конечное число
-function getNonFinite(result) {
+function getNonFinite(result) { // вычисление
   return isFinite(result) ? Math.round(result) : 0;
+}
+
+function updateArrayView(array) { // действие
+  $('[id*=row]').each((rowNum) => {
+    $(`[id*=floatInput${rowNum + 1}]`).each((colNum, el) => {
+      if (array[rowNum][rowNum] !== null) {
+        el.value = array[rowNum][colNum];
+      }
+    })
+  })
+}
+
+let DATA = [
+  [547000, 1246000, 829000, 815000, 492000, 186000, null, null, null, null],
+  [16136, 34325, 22405, 19878, 11472, 45377, null, null, null, null],
+  [33.9, 36.3, 37, 41, 41.9, 41.1, null, null, null, null],
+  [3647, 3473, 3435, 2426, 953, 5009, null, null, null, null],
+  [43.8, 41.6, 43.7, 46.7, 45.6, 46.2, null, null, null, null],
+]
+
+
+function updateArrayValue(row, col, newValue, array) { // вычисление
+  return withArrayCopy(array, function (arrayCopy) {
+    arrayCopy[row][col] = newValue;
+    return arrayCopy;
+  })
+}
+
+
+function withArrayCopy(array, callback) { // вычисление
+  const arrayCopy = _.cloneDeep(array)
+  return callback(arrayCopy)
 }
